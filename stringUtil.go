cc87abbc3d3a83add9017ee8126efeb3cc62dd18
Aurelien Castel <aureliencastel@hotmail.fr>
2016-12-17 20:01:10 +0100

    Added the HasOneOfSuffixes, a function that check if a string end with one of the string from a string slice.

---
+++ stringUtil.go
@@ -0,0 +1,12 @@
+package stringUtil
+
+import "strings"
+
+func HasOneOfSuffixes(str string, suffixes []string) bool {
+	for _, suffix := range suffixes {
+		if strings.HasSuffix(str, suffix) {
+			return true
+		}
+	}
+	return false
+}
